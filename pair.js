88670c031cf85e75114c2b20ee29cd310c544cbc
DML <daudymussa1705@gmail.com>
2025-06-28 12:26:03 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,12 +6,12 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Gifted_Tech,
+    default: France_King,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("maher-zubair-baileys");
+} = require("@whiskeysockets/baileys");
 
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
@@ -20,47 +20,53 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function GIFTED_MD_PAIR_CODE() {
+        async function FLASH_MD_PAIR_CODE() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let Pair_Code_By_Gifted_Tech = Gifted_Tech({
+            let Pair_Code_By_France_King = France_King({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
                 },
                 printQRInTerminal: false,
-                logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ["Chrome (Linux)", "", ""]
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                browser: Browsers.macOS('Chrome')
              });
-             if(!Pair_Code_By_Gifted_Tech.authState.creds.registered) {
+             if(!Pair_Code_By_France_King.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Gifted_Tech.requestPairingCode(num)
+                            const code = await Pair_Code_By_France_King.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            Pair_Code_By_Gifted_Tech.ev.on('creds.update', saveCreds)
-            Pair_Code_By_Gifted_Tech.ev.on("connection.update", async (s) => {
+            Pair_Code_By_France_King.ev.on('creds.update', saveCreds)
+            Pair_Code_By_France_King.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
                 } = s;
                 if (connection == "open") {
-                await delay(5000);
+                await delay(50000);
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                await delay(800);
+                await delay(8000);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
+               let session = await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, { text: ''+ b64data });
 
-               let GIFTED_MD_TEXT = `
+               let FLASH_MD_TEXT = `
+
+
+
+*_Session Connected By DML-MD*
+*_Made in 🇹🇿_*
+______________________________________
 ╔════════════════════════════╗
-  🌟 𝐃𝐌𝐋-𝐌𝐃 𝐕𝐄𝐑 3 𝐀𝐂𝐓𝐈𝐕𝐄! 🌟
+  🌟     𝐃𝐌𝐋-𝐌𝐃   𝐀𝐂𝐓𝐈𝐕𝐄!   🌟
 ╚════════════════════════════╝
-🔮 Session Connected @ ${time}
+🔮 Session Connected 
 
 ███████████████████████████
 🚀 𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗘𝗫𝗣𝗘𝗥𝗜𝗘𝗡𝗖𝗘 𝗔𝗖𝗧𝗜𝗩𝗔𝗧𝗘𝗗
@@ -75,47 +81,36 @@ router.get('/', async (req, res) => {
 💻 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥 𝗛𝗨𝗕
 ├─ 🌐 GitHub: github.com/MLILA17/DML-MD
 ├─ ⭐ Star | 🍴 Fork | 💻 Contribute
-└─ 📜 Docs: 255785591288
+└─ 📜 Docs: 255713541112
 
 🛠 𝗦𝗨𝗣𝗣𝗢𝗥𝗧
 ├─ 📧 Email: daudymussa1705@gmail.com
-├─ 📞 WhatsApp: wa.me/255785591288
+├─ 📞 WhatsApp: wa.me/255713541112
 └─ 🐛 Bug Report: /reportbug
-
-███████████████████████████
-🛠️ 𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬
-┌─ 🤖 DML-MD Engine v3.0
-├─ ⚡ AI-Powered Responses
-├─ 🔥 Instant Execution
-└─ 🌍 Open-Source Community
-
-❤️ 𝗦𝗢𝗪 𝗧𝗢 𝗦𝗨𝗣𝗣𝗢𝗥𝗧
-✓ 🌟 GitHub Star
-✓ 📲 Share with Friends
-✓ 💡 Suggest Features: /suggest
 
 ╔════════════════════════════╗
   💎 𝗗𝗠𝗟-𝗠𝗗 𝗧𝗘𝗣𝗘 /𝗛𝗘𝗟𝗣 𝗙𝗢𝗥 𝗠𝗘𝗡𝗨
-╚════════════════════════════╝`
- await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
+╚════════════════════════════╝
+`
+ await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id,{text:FLASH_MD_TEXT},{quoted:session})
  
 
         await delay(100);
-        await Pair_Code_By_Gifted_Tech.ws.close();
+        await Pair_Code_By_France_King.ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    GIFTED_MD_PAIR_CODE();
+                    FLASH_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
             console.log("service restated");
             await removeFile('./temp/'+id);
          if(!res.headersSent){
-            await res.send({code:"Service Unavailable"});
+            await res.send({code:"Service is Currently Unavailable"});
          }
         }
     }
-    return await GIFTED_MD_PAIR_CODE()
+    return await FLASH_MD_PAIR_CODE()
 });
 module.exports = router
